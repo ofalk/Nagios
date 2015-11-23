803d40afd4ebc7d9a7ab14b13ff7a6c0f80a9d16
Oliver Falk <oliver@linux-kernel.at>
2015-11-23 11:05:57 +0100

    Ingore some SSSD cache

--- check_procs_need_restart_by_ssh.pl
+++ check_procs_need_restart_by_ssh.pl
@@ -20,7 +20,7 @@ Hash::Merge::set_behavior('RIGHT_PRECEDENT');
 my $defconfig = Load('
 check_by_ssh: /opt/omd/versions/1.10/lib/nagios/plugins/check_by_ssh
 ssh_user: root
-command: lsof |grep -E \'lib|bin\' |grep DEL|grep -v /SYSV00000000 | cut -f 1 -d \' \' | sort -u
+command: lsof |grep -E \'lib|bin\' |grep DEL|grep -v /SYSV00000000 | grep -v /var/lib/sss/mc | cut -f 1 -d \' \' | sort -u
 ssh_opts: -oNumberOfPasswordPrompts=0 -oPasswordAuthentication=no -oStrictHostKeyChecking=no
 check_by_ssh_opts: -E -t 180
 ');
